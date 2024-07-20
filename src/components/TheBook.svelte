a2717be6e3a9dd7cbf302fa566e04c2740618bb4
jurajliska <jurinnko@gmail.com>
2024-07-20 14:51:37 +0200

    initial commit

---
+++ src/components/TheBook.svelte
@@ -0,0 +1,43 @@
+<script>
+    export let chosenBook
+
+</script>
+
+<style>
+    h2 {
+		color: #ff3e00;
+		font-size: 4.5em;
+		font-weight: 100;
+		font-weight: 900;
+		text-transform: uppercase;
+		text-shadow: 3px 3px 0 #000;
+		-webkit-text-stroke: 2px #710000;
+		letter-spacing: -2px;
+
+		position: absolute;
+		margin-top: 0;
+		left: 0;
+		right: 0;
+		margin-left: auto;
+		margin-right: auto;
+		top: 23vh;
+	}
+
+	h2 small {
+		color: #d2c312;
+		font-size: 0.65em;
+	}
+
+	h2 small::before {
+		text-transform: lowercase;
+		font-size: 0.75em;
+	}
+</style>
+
+<h2>
+    <span>{chosenBook.title}</span>
+
+    {#if chosenBook.page}
+        <small>{chosenBook.page}</small>
+    {/if}
+</h2>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,89 @@
+<script>
+    //components
+    import Wave from "../components/Wave.svelte";
+    import TheBook from "../components/TheBook.svelte";
+
+    //imports
+    import { tweened } from "svelte/motion";
+	import { cubicOut, circOut } from "svelte/easing";
+    import { shuffleArray } from "../functions.js"
+ 
+    //images
+    import hem from "../assets/hemingway.png";
+    import gun from "../assets/gun.png";
+    import anchor from "../assets/anchor.png"
+    import audio from "../assets/dsshotgn.wav"
+
+    import booky from "../books.js";
+
+
+    //vars
+    let waving = true
+    let books = booky
+
+    function gunAudio(){
+        const gunAudio = document.querySelector("audio")
+        gunAudio.play()
+    }
+    
+    //countdown
+    const progress = tweened(0, {
+		duration: 2500,
+		easing: circOut
+	});
+
+    //randomize (choose random book)
+    const chooseBook = () => {
+        shuffleArray(books)
+
+        let index = $progress === 0 ? books.length -1 : 0
+
+        progress.set(index).then(() => {
+            gunAudio()
+        })
+    }
+
+    //Move book from unread to read
+    const readBook = () => {
+        alert("readedbook")
+    }
+
+    $: chosenBook = books[Math.floor($progress)] || books[0]
+</script>
+
+<style>
+    label{
+        position: absolute;
+        top: 1em;
+        left: 1em;
+        font-size: 0.7em;
+    }
+</style>
+
+<header>
+    <h1>HEMINGWAYOVÁTOR-ROTÁTOR</h1>
+    <audio src={audio}></audio>
+
+    <label for="waving">
+        <input type="checkbox" id="waving" bind:checked={waving}>
+        waving
+    </label>
+</header>
+
+<main>
+    <TheBook {chosenBook} />
+
+    <section class="controls">
+        <img on:click={chooseBook} src={gun} alt="cool ass gun">
+        <img on:click={readBook} src={anchor} alt="sweet anchor">
+    </section>
+    
+</main>
+
+<footer class="water">
+    <Wave where = "back" animation="big" {waving} />
+    <Wave where = "midle" animation="tiny" {waving} />
+    <img class="hem" src={hem} alt="my boy ham"/>
+    <Wave where = "front" animation="small" {waving} />
+    
+</footer>
